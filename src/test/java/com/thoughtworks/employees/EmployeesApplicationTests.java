242aa54cc690d42c41a7fb321411c583d8c5f52f
Lucerne <lucerne_liu@foxmail.com>
2018-04-10 21:24:57 +0800

    test:add "should_return_employeeList_when_get_employees"

--- src/test/java/com/thoughtworks/employees/EmployeesApplicationTests.java
+++ src/test/java/com/thoughtworks/employees/EmployeesApplicationTests.java
@@ -1,25 +1,41 @@
 package com.thoughtworks.employees;
 
+import com.thoughtworks.employees.web.EmployeeController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest()
+@SpringBootTest(classes = MockServletContext.class)
 public class EmployeesApplicationTests {
+	private MockMvc mvc;
+	private RequestBuilder request;
+
+	@Before
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new EmployeeController()).build();
+		request = null;
+	}
 
 	@Test
-	public void contextLoads() {
+	public void should_return_employeeList_when_get_employees() throws Exception {
+		request = get("/employees");
+		mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}," +
+				"{\"id\":2,\"name\":\"小红\",\"age\":19,\"gender\":\"女\"}," +
+				"{\"id\":3,\"name\":\"小智\",\"age\":15,\"gender\":\"男\"}," +
+				"{\"id\":4,\"name\":\"小刚\",\"age\":16,\"gender\":\"男\"}," +
+				"{\"id\":5,\"name\":\"小霞\",\"age\":15,\"gender\":\"女\"}]")));
 	}
 
 }
