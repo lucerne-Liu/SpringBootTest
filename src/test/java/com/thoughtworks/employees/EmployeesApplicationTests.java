1ad8a51189ed0d1d350b9dc9bfa6de92d86fd43a
Lucerne <lucerne_liu@foxmail.com>
2018-04-10 21:42:57 +0800

    test:add "should_delete_employee_when_delete_by_id"

--- src/test/java/com/thoughtworks/employees/EmployeesApplicationTests.java
+++ src/test/java/com/thoughtworks/employees/EmployeesApplicationTests.java
@@ -9,9 +9,7 @@ import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -75,5 +73,16 @@ public class EmployeesApplicationTests {
     public void should_return_employee_when_search_by_id() throws Exception {
         request = get("/employees/1");
         mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("{\"id\":1,\"name\":\"小明\",\"age\":20,\"gender\":\"男\"}")));
+    }
+
+    @Test
+    public void should_delete_employee_when_delete_by_id() throws Exception {
+        request = delete("/employees/1");
+        mvc.perform(request).andExpect(content().string(equalTo("success")));
+        request = get("/employees");
+        mvc.perform(request).andExpect(status().isOk()).andExpect(content().string(equalTo("[{\"id\":2,\"name\":\"小红\",\"age\":19,\"gender\":\"女\"}," +
+                "{\"id\":3,\"name\":\"小智\",\"age\":15,\"gender\":\"男\"}," +
+                "{\"id\":4,\"name\":\"小刚\",\"age\":16,\"gender\":\"男\"}," +
+                "{\"id\":5,\"name\":\"小霞\",\"age\":15,\"gender\":\"女\"}]")));
     }
 }
